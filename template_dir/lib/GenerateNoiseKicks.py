71fef7fe37fd790d25933b7127982617f21a7745
natriant <nataliatriantafyllou@gmail.com>
2020-10-30 16:19:39 +0100

    first commit for function to generate kciks

---
+++ template_dir/lib/GenerateNoiseKicks.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def seconds2rad(my_t, my_f_rf):
+    phi = my_t*2*np.pi*my_f_rf
+    return phi
+
+
+def rad2sec(my_rad, my_f_rf):
+    sec = my_rad/(2*np.pi*my_f_rf)
+    return sec
+
+
+def modulated_rf_phase(my_A_sec, my_mod_period_s, my_f_rf, my_turns_max, my_f_rev):
+    # returns the signal of the modulated RF, here the CC. The amplitude in rad
+    A_rad = seconds2ad(my_A_sec, my_f_rf)  # amplitude of the RF modulation in rad
+    mod_freq_Hz = 1 / my_mod_period_s  # frequency of the RF phase modulation in Hz
+
+    turns = np.arange(my_turns_max)
+    time = turns/my_f_rev  # seconds
+
+    # Create the sin signal
+    my_signal = A_rad*np.sin(2*np.pi*mod_freq_Hz*time)
+    return my_signal
+
+
